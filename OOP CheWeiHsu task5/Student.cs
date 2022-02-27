0c0c9c2f0f96bb589263f9c00a6a87a896e14a4d
KingOfSpicy <sangowosan@gmail.com>
2022-02-27 09:06:41 +0200

    Add codes pages.

---
+++ OOP CheWeiHsu task5/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CheWeiHsu_task5
+{
+    class Student : IEquatable<Student>
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        //without constructor
+        //public Student(string name, string address, DateTime dateOfBirth)
+        //{
+        //    Name = name;
+        //    Address = address;
+        //    DateOfBirth = dateOfBirth;
+        //}
+
+        public Student(string name, string address, DateTime dateOfBirth)
+        {
+            Name = name;
+            Address = address;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Address: " + Address + ", Birthday: " + DateOfBirth.ToLongDateString();
+            //return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Student);
+        }
+
+        public bool Equals(Student other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   Address == other.Address &&
+                   DateOfBirth == other.DateOfBirth;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1069516934;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Address);
+            hashCode = hashCode * -1521134295 + DateOfBirth.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ OOP CheWeiHsu task5/testing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CheWeiHsu_task5
+{
+    class testing : IEquatable<testing>
+    {
+        public string question;
+        public string answer;
+
+        public testing(string question, string answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as testing);
+        }
+
+        public bool Equals(testing other)
+        {
+            return other != null &&
+                   question == other.question &&
+                   answer == other.answer;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -444598804;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(question);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(answer);
+            return hashCode;
+        }
+    }
+}
